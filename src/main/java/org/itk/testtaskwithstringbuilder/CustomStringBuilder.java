4e6a964bf144c71b6ab59710f9920d297ea52140
Canib-us <162473942+Canib-us@users.noreply.github.com>
2025-07-30 23:23:22 +0700

    Update CustomStringBuilder.java

--- src/main/java/org/itk/testtaskwithstringbuilder/CustomStringBuilder.java
+++ src/main/java/org/itk/testtaskwithstringbuilder/CustomStringBuilder.java
@@ -1,4 +1,47 @@
 package org.itk.testtaskwithstringbuilder;
 
+import java.util.Stack;
+
 public class CustomStringBuilder {
+    private final StringBuilder sb;
+    private final Stack<String> snapshot = new Stack<>();
+
+    public CustomStringBuilder(StringBuilder sb) {
+        this.sb = sb;
+    }
+
+    public CustomStringBuilder append(String s) {
+        saveSnapshot();
+        sb.append(s);
+        return this;
+    }
+
+    public CustomStringBuilder delete(int start, int end) {
+        saveSnapshot();
+        sb.delete(start, end);
+        return this;
+    }
+
+    public CustomStringBuilder insert(int offset, String s) {
+        saveSnapshot();
+        sb.insert(offset, s);
+        return this;
+    }
+
+    public CustomStringBuilder undo(){
+        if(!snapshot.isEmpty()){
+            String prevStr = snapshot.pop();
+            sb.setLength(0);
+            sb.append(prevStr);
+        }
+        return this;
+    }
+
+    public String toString() {
+        return sb.toString();
+    }
+
+    private void saveSnapshot() {
+        snapshot.push(sb.toString());
+    }
 }
