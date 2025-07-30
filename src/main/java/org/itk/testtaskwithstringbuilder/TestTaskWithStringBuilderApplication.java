fb92067027164d2d60b5b049844ffdef14250278
Canib-us <162473942+Canib-us@users.noreply.github.com>
2025-07-30 23:24:03 +0700

    Update TestTaskWithStringBuilderApplication.java

--- src/main/java/org/itk/testtaskwithstringbuilder/TestTaskWithStringBuilderApplication.java
+++ src/main/java/org/itk/testtaskwithstringbuilder/TestTaskWithStringBuilderApplication.java
@@ -8,6 +8,23 @@ public class TestTaskWithStringBuilderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestTaskWithStringBuilderApplication.class, args);
+
+        StringBuilder sb = new StringBuilder("Hello World");
+        CustomStringBuilder csb = new CustomStringBuilder(sb);
+
+        System.out.println(csb);
+
+        csb.append(", first append");
+        System.out.println(csb);
+
+        csb.delete(18, 24);
+        System.out.println(csb);
+
+        csb.undo();
+        System.out.println(csb);
+
+        csb.undo();
+        System.out.println(csb);
     }
 
 }
